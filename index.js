bfd3793092db374ec9d88f9a68777c9447d6b782
Fluke <fluke@getnuvo.com>
2025-07-15 15:53:33 +0700

    chore: update code to support more then 35 branches item

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
+#!/usr/bin/env node
 import { execSync } from "child_process";
 import chalk from "chalk";
 import readlineSync from "readline-sync";
 
 const exceptBranches = ["main"];
+
+const selectPaginationItem = (items) => {
+  if (!items || !items.length) {
+    return -1;
+  }
+
+  const MAX_ITEMS = 8,
+    MAX_PAGE_INDEX = Math.ceil(items.length / MAX_ITEMS) - 1;
+
+  let pageIndex = 0;
+  while (true) {
+    const PAGE_ITEMS = [];
+    let indexPrev = -1,
+      indexNext = -1;
+    if (pageIndex > 0) {
+      PAGE_ITEMS.push(`(PREVIOUS ${MAX_ITEMS} items)`);
+      indexPrev = PAGE_ITEMS.length - 1;
+    }
+    Array.prototype.push.apply(
+      PAGE_ITEMS,
+      items.slice(pageIndex * MAX_ITEMS, (pageIndex + 1) * MAX_ITEMS)
+    );
+    if (pageIndex < MAX_PAGE_INDEX) {
+      PAGE_ITEMS.push(
+        `(NEXT ${
+          pageIndex < MAX_PAGE_INDEX - 1
+            ? MAX_ITEMS
+            : items.length - MAX_ITEMS * (pageIndex + 1)
+        } item(s))`
+      );
+      indexNext = PAGE_ITEMS.length - 1;
+    }
+
+    console.log("\x1B[2J");
+    const index = readlineSync.keyInSelect(PAGE_ITEMS);
+    if (indexPrev !== -1 && index === indexPrev) {
+      pageIndex--;
+    } else if (indexNext !== -1 && index === indexNext) {
+      pageIndex++;
+    } else {
+      return index === -1
+        ? index
+        : index + pageIndex * MAX_ITEMS - (indexPrev === -1 ? 0 : 1);
+    }
+  }
+};
 
 const branchList = () => {
   let branches = execSync("git branch")
@@ -33,12 +80,7 @@ const deleteBranches = () => {
       return;
     }
     branches.unshift("all branches");
-
-    const selectedBranches = readlineSync.keyInSelect(
-      branches,
-      "Select branches to delete:",
-      { cancel: "Exit", guide: true, multiSelect: true }
-    );
+    const selectedBranches = selectPaginationItem(branches);
     if (selectedBranches === -1) {
       console.log("Exiting without deleting any branches.");
       return;
@@ -67,14 +109,15 @@ const deleteBranches = () => {
 
     const remainingBranches = branchList();
     console.log(
-      chalk.bgYellow(
+      chalk.green(
         remainingBranches.length > 0
-          ? `\nBraches remaining: ${remainingBranches?.join(", ")}`
+          ? `\nBraches remaining:\n${remainingBranches?.join("\n")}`
           : "\nNo branches remaining."
       )
     );
   } catch (error) {
-    console.error("Error retrieving current branch:", error.message);
+    console.log(error);
+    // console.error("Error retrieving current branch:", error.message);
   }
 };
 
