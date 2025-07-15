eaf3ca4c4d8696a29697e9764969a6d70ebeb068
Fluke <fluke@getnuvo.com>
2025-07-15 13:31:20 +0700

    feat: add git branch delete helper

--- index.js
+++ index.js
@@ -1,12 +1,81 @@
-const { execSync } = require("child_process");
+import { execSync } from "child_process";
+import chalk from "chalk";
+import readlineSync from "readline-sync";
 
-const main = () => {
-    try {
-        const output = execSync("git branch").toString().trim();
-        console.log(`Current branch: ${output}`);
-    } catch (error) {
-        console.error("Error retrieving current branch:", error.message);
+const exceptBranches = ["main"];
+
+const branchList = () => {
+  let branches = execSync("git branch")
+    .toString()
+    .trim()
+    .replace(/^\* /gm, "")
+    .split("\n")
+    .reduce((result, branch) => {
+      if (
+        branch &&
+        branch?.trim() &&
+        !exceptBranches?.includes(branch?.trim())
+      ) {
+        result.push(branch.trim());
+      }
+
+      return result;
+    }, []);
+
+  return branches;
+};
+
+const deleteBranches = () => {
+  try {
+    const branches = branchList();
+    if (branches.length === 0) {
+      console.log("No branches available to delete.");
+      return;
     }
-}
+    branches.unshift("all branches");
 
-main();
+    const selectedBranches = readlineSync.keyInSelect(
+      branches,
+      "Select branches to delete:",
+      { cancel: "Exit", guide: true, multiSelect: true }
+    );
+    if (selectedBranches === -1) {
+      console.log("Exiting without deleting any branches.");
+      return;
+    }
+
+    let branchesToDelete = [branches[selectedBranches]];
+    if (selectedBranches === 0) {
+      branchesToDelete = branches.slice(1);
+    }
+
+    const confirm = readlineSync.keyInYNStrict(
+      `Are you sure you want to delete the selected branches? (${branchesToDelete.join(
+        ", "
+      )})`
+    );
+
+    console.log(`Deleting branches: ${branchesToDelete}`);
+    if (confirm) {
+      for (const branchToDelete of branchesToDelete) {
+        if (branchToDelete) {
+          execSync(`git branch -D ${branchToDelete}`);
+          console.log(chalk.green(`\nDeleted branch: ${branchToDelete}`));
+        }
+      }
+    }
+
+    const remainingBranches = branchList();
+    console.log(
+      chalk.bgYellow(
+        remainingBranches.length > 0
+          ? `\nBraches remaining: ${remainingBranches?.join(", ")}`
+          : "\nNo branches remaining."
+      )
+    );
+  } catch (error) {
+    console.error("Error retrieving current branch:", error.message);
+  }
+};
+
+deleteBranches();
